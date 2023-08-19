19187bf4af26b46f73999cd842684c6713aa67f0
Stephen Alderman <stephen@alderman.uk>
2023-08-19 13:13:57 +0100

    🐛 Manually set basePath to fix GitHub pages
    Fixes issue with using next-pwa

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
     // trailingSlash: true,
     // Optional: Change the output directory `out` -> `dist`
     // distDir: 'dist',
+    basePath: '/damage-calcualtor',
 }
 
 module.exports = withPWA(nextConfig)
